fc5233f81cc3cc0a7e73d5da4029d9037de95ef7
Shashi Gowda <shashigowda91@gmail.com>
2014-12-27 00:55:14 +0530

    First commit!

--- README.md
+++ README.md
@@ -1,1 +1,146 @@
 # Reel
+
+***Capture computations.***
+
+## Quickstart
+
+### Installation
+```julia
+Pkg.add("Reel")
+```
+
+You will need `ffmpeg` and `imagemagick` installed to use Reel.
+
+### Usage
+
+Reel exports the all-important function `roll` which you can use to roll the camera on various arguments:
+
+```julia
+using Reel
+using Gadfly
+
+function render(t, dt)
+    # t is the time into the sequence
+    # dt is the time to advance for the next frame
+
+    # any expression that results in an object which can be
+    # rendered as png or jpg
+    plot([x -> sin(x+t*π), x -> cos(x+t*π)], 0, 6)
+end
+
+film = roll(render, fps=30, duration=2)
+
+write("output.gif", film) # Write to a gif file
+
+# Or
+write("file.webm", film) # Write to a webm video
+
+# Or
+write("file.mp4", film)  # An mp4 formatted video
+```
+
+The same thing can be concisely written with Julia's do syntax:
+
+```julia
+film = roll(fps=30, duration=2) do t, dt
+    plot([x -> sin(x+t*π), x -> cos(x+t*π)], 0, 6)
+end
+
+write("output.gif", film)
+```
+
+Note that the fps you specify to `roll` will be enforced no matter how long the computation actually takes: you set the speed at which you want to watch it.
+
+Reel can also render an anstract array of objects:
+
+```julia
+using Compose
+
+Compose.set_default_graphic_size(3inch, 3inch)
+
+# draw a regular n-gon
+ngon(n) = compose(context(units=UnitBox(-1, -1, 2, 2)), fill("lightblue"),
+                polygon([(cos(x), sin(x)) for x in π/2:2π/n:3.5π]))
+
+roll(map(ngon, vcat([3:10], [9:-1:3])), fps=5)
+```
+
+Reel exports `Frames`, a type for a collection of frames.
+
+`Frames` constructor takes a MIME type of the frames, and the fps at which the animation should be rendered. e.g.
+```julia
+frames = Frames(MIME("image/png"), fps=30)
+```
+
+You can push a new frames to a `Frames` object using `push!`.
+
+```julia
+# A Glider gun
+
+using Reel
+frames = Frames(MIME("image/png"), fps=2)
+
+g = glider_gun # initial board state
+for i=1:31
+    push!(frames, draw(g))
+    g = decidefate(g)
+end
+
+frames
+
+```
+
+You can render a frames object as usual:
+
+```julia
+write("output.webm", frames)
+```
+
+## In IJulia
+
+A call to `roll` returns a `Frames` object. A method `writemime(::IO, ::MIME{symbol("text/html"),::Frames)` orchestrates the rendering of a `Frames` object in IJulia. Any cell which results in a `Frames` results in the animation being rendered. By default, a webm video is generated. You can change this behavior:
+
+```julia
+Reel.set_output_type("gif") # or "mp4"
+```
+
+## They see me `roll`in', they hatin'
+
+```julia
+# particles in a box.
+
+using Color, Compose
+
+box(x) = let i = floor(x)
+    i%2==0 ? x-i : 1+i-x
+end
+
+colors = distinguishable_colors(10, lchoices=[82.])
+
+dots(points) = [(context(p[1], p[2], .03, .03), fill(colors[i%10+1]), circle())
+    for (i, p) in enumerate(points)]
+
+velocities = [(rand(), rand()) for i in 1:40]
+
+roll(fps=30, duration=10.0) do t, dt
+
+    compose(context(),
+            dots([map(v -> box(v*t + 0.5), (vx, vy)) for (vx, vy) in velocities])...)
+end
+
+```
+
+
+```julia
+# Derichilet Helmholtz equation using ApproxFun
+
+using ApproxFun
+
+B=dirichlet(d)
+Δ=lap(d)
+
+plots = [ApproxFun.contour(pdesolve([B,Δ+k*I],ones(4),150))
+            for k=vcat([1:1.0:100], [99:-1.0:1])]
+
+roll(plots, fps=24)
+```
--- src/Reel.jl
+++ src/Reel.jl
@@ -1,5 +1,163 @@
 module Reel
 
-# package code goes here
+using Compat
+
+import Base: write, push!, writemime
+export Frames, roll
+
+global _output_type = "webm"
+function set_output_type(t)
+    global _output_type
+    _output_type = t
+end
+
+type Frames{M <: MIME}
+    tmpdir::AbstractString
+    length::Uint
+    fps::Float64
+    rendered::Union(Nothing, AbstractString)
+    function Frames(; fps=30)
+        tmpdir = tempname()
+        mkdir(tmpdir)
+        new(tmpdir, 0, fps, nothing)
+    end
+end
+Frames{M <: MIME}(m::M; fps=30) = Frames{M}(fps=fps)
+
+extension(m::MIME"image/png") = "png"
+extension(m::MIME"image/jpeg") = "jpg"
+extension(s::AbstractString) = split(s, ".")[end] 
+
+function writeframe(filename, mime::MIME, frame)
+    file = open(filename, "w")
+    writemime(file, mime, frame)
+    close(file)
+end
+
+
+function push!{M}(frames::Frames{M}, x)
+    frames.length += 1
+    writeframe(
+        joinpath(frames.tmpdir,
+            string(frames.length, ".", extension(M()))),
+        M(), x)
+    frames
+end
+
+const mime_ordering = map(MIME, [
+    "image/png",
+    "image/svg",
+    "image/jpeg",
+    "text/html"
+])
+
+function bestmime(x)
+    for m in mime_ordering
+        if mimewritable(m, x)
+            return m
+        end
+    end
+end
+
+function write{M}(f::AbstractString, frames::Frames{M}; fps=frames.fps)
+    # TODO: more ffmpeg options
+    dir = frames.tmpdir
+    ext = extension(M())
+    oext = extension(f)
+
+    if oext == "gif"
+        # The maximum delay widely supported by clients is 2 ticks (100 ticks per sec)
+        #delay = max(round(100/fps), 2) |> int
+        args = reduce(vcat, [[joinpath("$dir", "$i.$ext"), "-delay", "1x$fps", "-alpha", "remove"] for i in 1:frames.length])
+        imagemagick_cmd = `convert $args $f`
+        #run(`convert -alpha remove $dir/*.$ext -delay $delay $f`)
+        run(imagemagick_cmd)
+        frames.rendered = f
+    else
+        run(`ffmpeg -r $fps -f image2 -i $dir/%d.$ext $f` |> DevNull .> DevNull)
+        frames.rendered = f
+    end
+end
+
+
+### Roll teh camraz! ###
+
+function roll(render::Union(Function, Type);
+                 fps=30, duration=5.0)
+    t      = 0.0
+    dt     = 1.0 / fps
+    steps  = duration / dt
+    frame  = render(t, dt)
+    mime   = bestmime(frame)
+    frames = Frames(mime)
+    push!(frames, frame)
+
+    for i=2:steps
+        push!(frames, render(t, dt))
+        t += dt
+    end
+    frames
+end
+
+function roll(frames::AbstractArray; fps=30)
+    @assert length(frames) > 1
+    mime = bestmime(frames[1])
+    reduce(push!, Frames(mime, fps=fps), frames)
+end
+
+function newname!(ext)
+    string("reel-", rand(Uint), ".", ext)
+end
+
+#### writemimes ####
+# TODO: research the crap out of this
+
+function writehtml(io, file, file_type)
+    randstr = rand(Uint)
+    if file_type == "gif"
+        write(io, """<img src="$file?$randstr" />""")
+    elseif file_type == "webm"
+        write(io, """<video autoplay controls><source src="$file?$randstr" type="video/webm"></video>""")
+    elseif file_type == "mp4"
+        write(io, """<video autoplay controls><source src="$file?$randstr" type="video/mp4"></video>""")
+    end
+end
+
+function writemime(io::IO, ::MIME"text/html", frames::Frames)
+    # If IJulia is present, see if the frames are rendered
+    global _output_type
+    if frames.rendered == nothing || !isfile(frames.rendered) || extension(frames.rendered) != _output_type
+        fn = newname!(_output_type)
+        write(fn, frames)
+        frames.rendered = fn
+    end
+
+    if isdefined(Main, :IJulia)
+        if frames.rendered != nothing
+            if beginswith(abspath(frames.rendered), abspath(""))
+                # we can serve it right up from here.
+                file = replace(abspath(frames.rendered), abspath(""), "")
+                writehtml(io, "files/" * file, extension(file))
+            else
+                # the file is unreachable to the server, so we symlink or copy it.
+                fn = newname!(_output_type)
+                try
+                    symlink(frames.rendered, fn)
+                catch
+                    cp(frames.rendered, fn)
+                end
+                writehtml(io, "files/" * fn, extension(fn))
+            end
+        end
+    else
+        # TODO base64 encode, so many possible types!
+        # write(io, """<video autoplay controls><source src="files/$(frames.rendered)" type="video/webm"></video>""")
+    end
+end
+
+function cleanup()
+    run(`rm reel-*.*`)
+end
+
 
 end # module
