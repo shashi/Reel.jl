227d379abf1b7772659762ada43a20dd995f5ece
Tim Wheeler <timwheeleronline@gmail.com>
2016-10-24 20:57:53 +0200

    temporarily commenting out webm and mp4

--- test/runtests.jl
+++ test/runtests.jl
@@ -16,8 +16,8 @@ let
     film = roll(render, fps=30, duration=2)
 
     write("output.gif", film) # Write to a gif file
-    write("file.webm", film) # Write to a webm video
-    write("file.mp4", film)  # An mp4 formatted video
+    # write("file.webm", film) # Write to a webm video
+    # write("file.mp4", film)  # An mp4 formatted video
 end
 
 
@@ -51,5 +51,5 @@ let
     end
 
     write("output.gif", frames)
-    write("output.webm", frames)
+    # write("output.webm", frames)
 end
